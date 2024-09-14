75ffddfa0e9c47612c38abab1a8b8549a65d7972
Dhatri Manaswini <dhatrimanaswini@gmail.com>
2024-09-14 22:06:04 +0530

    del app.py duplicate

--- application.py
+++ application.py
@@ -52,4 +52,4 @@ def predict():
         return render_template('index.html', res=res)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
+    app.run(host="0.0.0.0")        
